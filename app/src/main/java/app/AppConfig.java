d0e02138a84b0dd346365642dc7719eed46d9a91
Shraddha <shraddhasahu92@gmail.com>
2017-07-22 17:13:27 +0530

    Initial commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Shraddha on 22-07-2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.94/login";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.94/register";
+}
