8672ddeb7826000dd3e7d7f1b61f840b0e6a6e6d
Shraddha <shraddhasahu92@gmail.com>
2017-07-23 11:36:42 +0530

    URL modified

--- app/src/main/java/app/AppConfig.java
+++ app/src/main/java/app/AppConfig.java
@@ -9,5 +9,5 @@ public class AppConfig {
     public static String URL_LOGIN = "http://192.168.0.94:8080/login";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.94:80880/register";
+    public static String URL_REGISTER = "http://192.168.0.94:8080/register";
 }
